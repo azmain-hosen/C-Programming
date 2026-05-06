694a9dc3d443cfe43d90bdd5efc39b97d13976fd
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-06 23:37:47 +0600

    added hexadecimal_to_decimal.c

---
+++ Basics/Conversions/hexadecimal_to_decimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Hexadecimal number : ");
+    scanf("%x", &number);
+
+    printf("Decimal number : %d\n", number);
+
+    return 0;
+}
