1925bb7d26594ec8cdd9dcaed68745b42b82cb66
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-06 19:35:04 +0600

    added decimal_to_octal.c

---
+++ Basics/Conversions/decimal_to_octal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Decimal number : ");
+    scanf("%d", &number);
+
+    printf("Octal number : %o\n", number);
+
+    return 0;
+}
