3604597f58f97f17465f4b3b7610109396c92c0d
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-06 23:30:03 +0600

    added octal_to_hexadecimal.c

---
+++ Basics/Conversions/octal_to_hexadecimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Octal number : ");
+    scanf("%o", &number);
+
+    printf("Hexadecimal number : %x\n", number);
+
+    return 0;
+}
