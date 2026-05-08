06918f3fbe9733ff43d2f90966e5cd89f7c6d7e8
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-08 14:55:28 +0600

    added lower-to-uppercase.c

---
+++ Basics/Conversions/lower-to-uppercase.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    char lower_ch;
+
+    printf("Enter any lowercase character : ");
+    scanf("%c", &lower_ch);
+
+    printf("The uppercase character : %c", lower_ch - 32);
+
+    return 0;
+}
