f059fc5774c288e2bf6d5abeb2eb40fa2076abaf
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-15 20:59:35 +0600

    added pos_neg.c

---
+++ Basics/Control_Flow/simple_if_else/pos_neg.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    printf("Enter any number : ");
+    scanf("%d", &x);
+
+    if (x >= 0)
+    {
+        printf("Number is positive\n");
+    }
+    else
+    {
+        printf("Number is negative\n");
+    }
+
+    return 0;
+}
