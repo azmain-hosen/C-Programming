fa8e019ba58a7a4c1d34de82787e58012aadf2b8
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-15 17:45:42 +0600

    added if_else_statement.c

---
+++ Basics/Control_Flow/simple_if_else/if_else_statement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+
+    if (x <= 2)
+    {
+        printf("Success!\n");
+    }
+    else
+    {
+        printf("Failure!\n");
+    }
+    return 0;
+}
