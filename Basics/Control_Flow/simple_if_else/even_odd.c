333498c6a5d4973879d6326946cfcd88c9875387
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-15 18:08:22 +0600

    added even_odd.c

---
+++ Basics/Control_Flow/simple_if_else/even_odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 50;
+
+    if (x % 2 == 0)
+    {
+        printf("%d is even number\n", x);
+    }
+    else
+    {
+        printf("%d is odd number\n", x);
+    }
+    return 0;
+}
