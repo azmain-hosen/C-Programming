c6a1abf93474cdc58742e72a6241e78ba5a2c24d
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-16 21:38:59 +0600

    update grade_system.c

--- Basics/Control_Flow/if_else_if/grade_system.c
+++ Basics/Control_Flow/if_else_if/grade_system.c
@@ -2,5 +2,40 @@
 
 int main()
 {
+    int marks;
+
+    printf("Enter your marks : ");
+    scanf("%d", &marks);
+
+    if (marks >= 80)
+    {
+        printf("You got A+\n");
+    }
+    else if (marks >= 70)
+    {
+        printf("You got A\n");
+    }
+    else if (marks >= 60)
+    {
+        printf("You got A-\n");
+    }
+    else if (marks >= 50)
+    {
+        printf("You got B\n");
+    }
+    else if (marks >= 40)
+    {
+        printf("You got C\n");
+    }
+    else if (marks >= 33)
+    {
+        printf("You got D\n");
+    }
+    else
+    {
+        printf("You failed\n");
+    }
     
+
+    return 0;
 }
