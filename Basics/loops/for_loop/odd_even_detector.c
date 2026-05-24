071b6509e219d6059ca8572aef159bd52b971fb8
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-24 11:39:37 +0600

    update odd_even_detector

---
+++ Basics/loops/for_loop/odd_even_detector.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("\n%d is even number\n", i);
+        }
+        else
+        {
+            printf("\n%d is odd number\n", i);
+        }
+    }
+
+    return 0;
+}
