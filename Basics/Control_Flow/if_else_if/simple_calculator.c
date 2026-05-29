a52967ba8d8c360733937381d70754aa6565793a
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-29 21:30:08 +0600

    update simple_calculator.c

--- Basics/Control_Flow/if_else_if/simple_calculator.c
+++ Basics/Control_Flow/if_else_if/simple_calculator.c
@@ -2,13 +2,34 @@
 
 int main()
 {
-    int num1, num2;
+    long double num1, num2;
+    char op;
 
     printf("Enter your first number : ");
-    scanf("%d", &num1);
+    scanf("%Lf", &num1);
 
-    printf("Enter your second number : ");
-    scanf("%d", &num2);
+    printf("\nEnter a operator : ");
+    scanf(" %c", &op);
 
+    printf("\nEnter your second number : ");
+    scanf("%Lf", &num2);
+
+    if (op == '+')
+    {
+        printf("\nThe result is : %.2Lf\n" ,num1 + num2);
+    }
+    else if (op == '-')
+    {
+        printf("\nThe result is : %.2Lf\n" ,num1 - num2);
+    }
+    else if (op == '*')
+    {
+        printf("\nThe result is : %.2Lf\n" ,num1 * num2);
+    }
+    else if (op == '/')
+    {
+        printf("\nThe result is : %.2Lf\n" ,num1 / num2);
+    }
     
+    return 0;
 }
