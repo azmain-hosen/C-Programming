2b4ddfa189ae3cde9e69e82ec2b9dfb0970ecf71
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-29 20:42:01 +0600

    added simple_calculator.c

---
+++ Basics/Control_Flow/if_else_if/simple_calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter your first number : ");
+    scanf("%d", &num1);
+
+    printf("Enter your second number : ");
+    scanf("%d", &num2);
+
+    
+}
