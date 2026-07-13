bda3e3638fc625463c3b85a260be5169a6da03d1
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-07-13 19:58:32 +0600

    update 04-formatted_output.c

---
+++ 01-basics/01-first_program/04-formatted_output.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n==========================\n       C Programming\n==========================\n\nName : Azmain Hosen\nAge : 18\nCountry : Bangladesh\n\nWelcome to C Programming!\n\n");
+
+    return 0;
+}
