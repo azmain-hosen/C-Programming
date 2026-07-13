01e9fa843448606b3f6dffbb2401011f6d056c40
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-07-13 21:16:35 +0600

    update 03-escape_sequences

---
+++ 01-basics/01-first_program/03-escape_sequences.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n==================================\n\tEscape Sequences\n==================================\n\nName\t: Azmain\nAge\t: 18\n\nPath\t: C:\\Users\\Azmain\\Desktop\n\nMessage : \"Welcome to C Programming\"\n\nGrade\t: \'A\'\n");
+
+    return 0;
+}
