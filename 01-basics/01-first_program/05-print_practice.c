b883106e6bff5993918b939c9ed54a564fc730d7
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-14 01:07:48 +0600

    Add print_practice program
    Adds a C example demonstrating formatted output and includes the compiled binary in the output folder.

---
+++ 01-basics/01-first_program/05-print_practice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n===========================\n"
+           "\tMy Profile\n"
+           "===========================\n"
+           "\nName\t: Alex\n"
+           "Age\t: 18\n"
+           "Country : Bangladesh\n\n"
+           "Message : \"I am learning C program.\"\n"
+           "Path\t: C:\\User\\Alex\n\n");
+
+    return 0;
+}
