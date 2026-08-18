7a6862e9bc2a26900ac97e5f36b5982103f102e5
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-18 20:30:31 +0600

    Add signed and unsigned integer example

---
+++ 01-basics/02-variables_and_data-types/03-type_modifiers/01-signed_unsigned.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    signed int a = -45;
+    signed int b = 45;
+    unsigned int c = 43;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("c = %u\n", c);
+
+    return 0;
+}
