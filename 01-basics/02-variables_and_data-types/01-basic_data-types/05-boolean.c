a9fb3718d2a49bcdcef73f2fe596cd3557c98ce9
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-18 18:12:35 +0600

    Add boolean example program demonstrating boolean data type

--- 01-basics/02-variables_and_data-types/01-basic_data-types/05-boolean.c
+++ 01-basics/02-variables_and_data-types/01-basic_data-types/05-boolean.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int main(void)
+{
+    bool is_student = true;
+    bool is_married = false;
+
+    printf("Is student  :  %d\n", is_student);
+    printf("Is married  :  %d\n", is_married);
+
+    return 0;
+}
