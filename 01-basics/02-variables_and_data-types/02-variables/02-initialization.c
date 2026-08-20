0c3046f9b103585e8885dc43e89470e8171ee8f0
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-20 22:43:39 +0600

    Add variable initialization example

---
+++ 01-basics/02-variables_and_data-types/02-variables/02-initialization.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 18;
+    float height = 5.8f;
+    double pi = 3.1415926535;
+    char grade = 'A';
+
+    printf("%d\n", age);
+    printf("%f\n", height);
+    printf("%lf\n", pi);
+    printf("%c\n", grade);
+
+    return 0;
+}
