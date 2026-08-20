f7bc8231ba202df97bf74476a96ed21b9add93a7
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-20 22:17:32 +0600

    update 04-type_ranges/01-integer_ranges.c added some file

---
+++ 01-basics/02-variables_and_data-types/04-type_ranges/01-integer-ranges.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+    printf("\nINT_MIN : %d\n", INT_MIN);
+    printf("INT_MAX : %d\n", INT_MAX);
+    printf("UINT_MAX : %u\n", UINT_MAX);
+    
+    return 0;
+}
