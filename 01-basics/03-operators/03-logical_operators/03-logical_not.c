3b7215f82ff2c0cf9991bf817377ce083533a238
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-26 19:10:18 +0600

    Add logical NOT operator example

---
+++ 01-basics/03-operators/03-logical_operators/03-logical_not.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int is_raining = 0;
+
+    int result = !is_raining;
+
+    printf("Logical Operator: !\n");
+    printf("---------------------\n");
+    printf("!Is Raining = %d\n", result);
+
+    return 0;
+}
