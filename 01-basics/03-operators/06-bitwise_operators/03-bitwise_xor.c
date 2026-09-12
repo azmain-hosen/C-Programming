d518b4ff15eb48e024a41b2583363f212c1902a1
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-09-12 15:00:59 +0600

    Add bitwise XOR operator example

---
+++ 01-basics/03-operators/06-bitwise_operators/03-bitwise_xor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int first_number = 5;
+    int second_number = 3;
+
+    int result = first_number ^ second_number;
+
+    printf("Bitwise XOR: %d ^ %d = %d\n",
+           first_number, second_number, result);
+
+    return 0;
+}
